0264af5ba60a1eda5001d834e07df7a684faf0e3
Pawla Homsi <paola.homsi24@gmail.com>
2022-11-17 12:00:13 +0200

    Time: 182 ms (59.75%), Space: 40 MB (64.34%) - LeetHub

--- 0268-missing-number/0268-missing-number.cs
+++ 0268-missing-number/0268-missing-number.cs
@@ -1,13 +1,13 @@
 public class Solution {
     public int MissingNumber(int[] nums) {
-        int result = 0;
+        int result = nums.Length;
         
         for(int i = 0; i < nums.Length; i++){
             
             result = result ^ nums[i] ^ i;
             
         }
-        return result ^ nums.Length;
+        return result;
         
     }
 }
