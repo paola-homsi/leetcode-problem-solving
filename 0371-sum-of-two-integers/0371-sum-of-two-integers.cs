fbe87b2b130d419fd01a6ac152084efe2b617a9f
Pawla Homsi <paola.homsi24@gmail.com>
2022-11-19 16:24:38 +0200

    Time: 46 ms (21.08%), Space: 25.4 MB (30.97%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cs
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cs
@@ -1,8 +1,8 @@
 public class Solution {
     public int GetSum(int a, int b) {
-        
+        int result = 0;
         while(b != 0){
-            int result = a ^ b;
+            result = a ^ b;
             b = (a&b) << 1;
             
             a = result;
